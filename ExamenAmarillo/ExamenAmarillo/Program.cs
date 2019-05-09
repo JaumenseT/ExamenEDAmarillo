d216c48ff5c7f5ea50cef02fa84ebead47d0930b
JhernKR <josephernandez108@gmail.com>
2019-05-09 16:08:47 +0200

    Establecidos los datos en el program.

--- ExamenAmarillo/ExamenAmarillo/Program.cs
+++ ExamenAmarillo/ExamenAmarillo/Program.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace ExamenAmarillo {
     class Program {
         static void Main(string[] args) {
-            Cliente cliente = new Cliente();
+            Cliente cliente = new Cliente("Antonio", "4567892Y");
+            Cliente.SumaSaldo(234);
+            Cliente.RestaSaldo(569);
+
+            Console.WriteLine("El cliente con Nombre: " + cliente.Nombre + " dni: " + cliente.Dni + "\n" + "Saldo: " + cliente.Saldo + " Positivo/Negativo: " + Cliente.PositivoNegativo(cliente.Saldo));
+            Console.ReadKey();
         }
     }
 }
